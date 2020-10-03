2453ee30832317398f43cad04bbb29baa57f2097
Jameel Amir <jameelozbiz@gmail.com>
2020-10-03 20:06:16 +0300

    changed usersGetRequest function name

--- src/store.js
+++ src/store.js
@@ -1,10 +1,10 @@
 import { store } from "@risingstack/react-easy-state";
 import axios from "axios";
-const Thingy = async () => {
+const usersGetRequest = async () => {
   const data = await axios.get("https://jsonplaceholder.typicode.com/users");
   myStore.contacts = data.data;
 };
-Thingy();
+usersGetRequest();
 const myStore = store({
   contacts: [],
 });
